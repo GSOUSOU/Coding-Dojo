2d71307da7fa852f407b19b419886b3ffeba9945
GSOUSOU <guied.souad2020@gmail.com>
2023-12-09 18:21:46 +0100

    Liking Likes Part I

---
+++ week3/day2/core/Liking Likes/part1/script.js
@@ -0,0 +1,8 @@
+// Getting the number of likes
+var likes_count = document.querySelector("#likes_count");
+
+// A function that increments the value in likes_count by 1
+function increment(){
+    /*  console.log(likes_count);*/
+  likes_count.innerText++;
+}
