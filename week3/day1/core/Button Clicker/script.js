a925d392288fc1e03554b1108a2c0af29e617e0c
GSOUSOU <guied.souad2020@gmail.com>
2023-12-09 09:40:05 +0100

    buttonclickCOMMIT

---
+++ week3/day1/core/Button Clicker/script.js
@@ -0,0 +1,12 @@
+/*******change text ta say "logout" when the button is clicked**** */
+
+function turnlogout(element) {
+    element.innerText = "Logout";
+}
+
+/*******remove the button when it is clicked**** */
+function hide(element) {
+    element.remove();
+}
+
+
