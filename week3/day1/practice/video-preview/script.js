02b3800c5fe84d66db87ecbf04d00a224ff7f5c3
GSOUSOU <guied.souad2020@gmail.com>
2023-12-09 14:00:06 +0100

    videolabcommit

---
+++ week3/day1/practice/video-preview/script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+/*var myVideo=document.getElementById("video1"); */
+function playingVideo(myVideo) {
+    myVideo.play();   
+}
+
+function pausingVideo(myVideo) {
+    myVideo.pause();   
+}
