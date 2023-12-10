5d19c3a4141efbf27050473e6f23d68496f1e555
GSOUSOU <guied.souad2020@gmail.com>
2023-12-10 11:41:56 +0100

    Liking Likes Part II correction

--- week3/day2/core/Liking Likes/part2/script.js
+++ week3/day2/core/Liking Likes/part2/script.js
@@ -1,28 +1,20 @@
 
+var likes_count1 = document.querySelector("#likes_count1");
 
-// A function that increments the value in likes_count by 1
-function increment(element){
-   console.log(element);
-  var idbuttoncliked=element.id;
- console.log(idbuttoncliked);
- if(idbuttoncliked==btnlikes1){
-    likes_count1 = document.querySelector("#likes_count1");
-    console.log(likes_count1);
-  /*  */
+
+function increment1(){
   likes_count1.innerText++;
+}
 
- }
- else if (idbuttoncliked==btnlikes2){
-    likes_count2 = document.querySelector("#likes_count2");
-    console.log(likes_count2);
-    /**/
-    likes_count2.innerText++;
- }else{
-    likes_count3 = document.querySelector("#likes_count3");
-   /*  */
-   console.log(likes_count3);
+var likes_count2 = document.querySelector("#likes_count2");
+function increment2(){
+     likes_count2.innerText++;
+}
+
+     varlikes_count3 = document.querySelector("#likes_count3");
+    function increment3(){
+
    likes_count3.innerText++;
  }
     
     
-}
